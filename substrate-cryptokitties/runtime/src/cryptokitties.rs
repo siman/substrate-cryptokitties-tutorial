83bd654e8dbea8a8f421338bdfc5159814fd7fc5
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-18 17:27:17 +0100

    Final code for storing a struct

--- substrate-cryptokitties/runtime/src/cryptokitties.rs
+++ substrate-cryptokitties/runtime/src/cryptokitties.rs
@@ -1,39 +1,40 @@
 use srml_support::{StorageMap, dispatch::Result};
 use system::ensure_signed;
-// ACTION: Continue to add required libraries here
-
+use runtime_primitives::traits::{As, Hash};
+use rstd::prelude::*;
 
 #[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
 pub struct Kitty<Hash, Balance> {
-    // ACTION: Define the properties of your kitty struct here
-    //      - `id` as a `Hash`
-    //      - `name` as a `Vec<u8>`
-    //      - `dna` as a `Hash`
-    //      - `price` as a `Balance`
-    //      - `gen` as a `u64`
+    id: Hash,
+    name: Vec<u8>,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
 }
 
 pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // ACTION: Update this storage item to store a `Kitty<T::Hash, T::Balance>`
-        Value: map T::AccountId => u64;
+        OwnedKitty: map T::AccountId => Kitty<T::Hash, T::Balance>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-        fn set_value(origin, name: Vec<u8>) -> Result {
+        fn create_kitty(origin, name: Vec<u8>) -> Result {
             let sender = ensure_signed(origin)?;
 
-            // ACTION: Create a `Kitty` object named `new_kitty` here
-            //      HINT: You can generate a hash with `<T as system::Trait>::Hashing::hash_of(&0)`
-            //            And you can generate a `0` balance with `<T::Balance as As<u64>>::sa(0)`
+            let new_kitty = Kitty {
+                                id: <T as system::Trait>::Hashing::hash_of(&0),
+                                name: name,
+                                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                                price: <T::Balance as As<u64>>::sa(0),
+                                gen: 0,
+                            };
 
-            // ACTION: Update this function to store your `new_kitty`
-            <Value<T>>::insert(sender, value);
+            <OwnedKitty<T>>::insert(&sender, new_kitty);
 
             Ok(())
         }
