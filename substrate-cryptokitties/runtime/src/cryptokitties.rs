8af4323a9c5579992b0156c4fbc44be8547c72dd
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-18 17:50:04 +0100

    Final code after the refactoring

--- substrate-cryptokitties/runtime/src/cryptokitties.rs
+++ substrate-cryptokitties/runtime/src/cryptokitties.rs
@@ -51,32 +51,10 @@ decl_module! {
 
         fn create_kitty(origin, name: Vec<u8>) -> Result {
             let sender = ensure_signed(origin)?;
-
-            // ACTION: Move this `owned_kitty_count` and `new_owned_kitty_count` logic into the `_mint()` function
-            let owned_kitty_count = Self::owned_kitty_count(&sender);
-
-            let new_owned_kitty_count = match owned_kitty_count.checked_add(1) {
-                Some(c) => c,
-                None => return Err("Overflow adding a new kitty to account balance"),
-            };
-
-            // ACTION: Move this `all_kitties_count` and `new_all_kitties_count` logic into the `_mint()` function
-            let all_kitties_count = Self::all_kitties_count();
-
-            let new_all_kitties_count = match all_kitties_count.checked_add(1) {
-                Some (c) => c,
-                None => return Err("Overflow adding a new kitty to total supply"),
-            };
-
-            // `nonce` and `random_hash` generation can stay here
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
                                 .using_encoded(<T as system::Trait>::Hashing::hash);
 
-            // ACTION: Move this collision check to the `_mint()` function
-            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
-
-            // Creating the `Kitty` object can stay here
             let new_kitty = Kitty {
                                 id: random_hash,
                                 name: name,
@@ -85,23 +63,9 @@ decl_module! {
                                 gen: 0,
                             };
 
-            // ACTION: Move all of the kitty related storage updates to the `_mint()` function
-            <Kitties<T>>::insert(random_hash, new_kitty);
-            <KittyOwner<T>>::insert(random_hash, &sender);
-
-            <AllKittiesArray<T>>::insert(all_kitties_count, random_hash);
-            <AllKittiesCount<T>>::put(new_all_kitties_count);
-            <AllKittiesIndex<T>>::insert(random_hash, all_kitties_count);
-
-            <OwnedKittiesArray<T>>::insert((sender.clone(), owned_kitty_count), random_hash);
-            <OwnedKittiesCount<T>>::insert(&sender, new_owned_kitty_count);
-            <OwnedKittiesIndex<T>>::insert(random_hash, owned_kitty_count);
+            Self::_mint(sender, random_hash, new_kitty)?;
             
-            // Nonce update can stay here
             <Nonce<T>>::mutate(|n| *n += 1);
-
-            // ACTION: Move this event to the `_mint()` function
-            Self::deposit_event(RawEvent::Created(sender, random_hash));
 
             Ok(())
         }
@@ -110,12 +74,34 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     fn _mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
-        // ACTION: Refactored code goes here
-        //      NOTE: Some variables have been renamed to generalize the function
-        //
-        // In the end, you should remember add the following call to `create_kitty()`:
-        // `Self::_mint(sender, random_hash, new_kitty)?;`
-        //      HINT: This `_mint()` function has checks which could fail AND will write to storage, so place it carefully
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        let owned_kitty_count = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count = match owned_kitty_count.checked_add(1) {
+            Some(c) => c,
+            None => return Err("Overflow adding a new kitty to account balance"),
+        };
+
+        let all_kitties_count = Self::all_kitties_count();
+
+        let new_all_kitties_count = match all_kitties_count.checked_add(1) {
+            Some (c) => c,
+            None => return Err("Overflow adding a new kitty to total supply"),
+        };
+
+        <Kitties<T>>::insert(kitty_id, new_kitty);
+        <KittyOwner<T>>::insert(kitty_id, &to);
+
+        <AllKittiesArray<T>>::insert(all_kitties_count, kitty_id);
+        <AllKittiesCount<T>>::put(new_all_kitties_count);
+        <AllKittiesIndex<T>>::insert(kitty_id, all_kitties_count);
+
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
 
         Ok(())
     }
