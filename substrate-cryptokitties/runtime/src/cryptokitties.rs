1c720c885e8b86d4a163555a340f46974dbcead9
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-18 17:25:03 +0100

    Final code for setting a value

--- substrate-cryptokitties/runtime/src/cryptokitties.rs
+++ substrate-cryptokitties/runtime/src/cryptokitties.rs
@@ -1,12 +1,11 @@
-use srml_support::dispatch::Result;
-// ACTION: import any required libraries here
-//      HINT: Use errors from the Rust compiler to help you
+use srml_support::{StorageValue, dispatch::Result};
+use system::ensure_signed;
 
 pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // ACTION: create a storage item named `Value` which stores a `u64`
+        Value: u64;
     }
 }
 
@@ -14,10 +13,10 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
         fn set_value(origin, value: u64) -> Result {
-            // ACTION: "Ensure" that the transaction is signed
+            let _sender = ensure_signed(origin)?;
 
-            // ACTION: "Put" the value into storage
-            
+            <Value<T>>::put(value);
+
             Ok(())
         }
     }
